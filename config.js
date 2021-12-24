0ecd6191561de26006d9d615b8db2aeb11eb51f8
teddie2610 <96612697+teddie2610@users.noreply.github.com>
2021-12-24 15:04:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,27 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "For bạn cr nqu zl",
+    introTitle: 'Vinh',
+    introDesc: `Baby em khiến con tim này trằn trọc
+Thao thức bao đêm ngày sàng lọc
+Từng lời ca như đang mong
+Một vòng tay dang rộng
+Cho nỗi cô đơn dày xéo đêm khuya
+Biến tan khi lặng thinh, nghe những chiếc hôn
+Từ đầu dây bên kia
+Vì...
+Ánh mắt ấy đã khiến anh rơi vào trong cơn u mê
+Làm trái tim thổn thức bao đêm ngày
+Mà chẳng được ngủ nghê`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Thì ra tương tư là thế này :0',
+    desc: '24/7 nghĩ đến Vinh uwu ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình? :>',
+    btnReply: 'Tại bạn vừa hiền vừa cute á',
+    reply: 'Thích rồi yêu rồi mỗi tội là t không nói',
+    mess: 'Mình biết mà 😏',
+    messDesc: 'Mai cho ôm miếng nka',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/teddie.anh' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
